44cd4b48a796c038bd18ffc531c3a93ea53f9500
anamartinezfernandez <anamartinezf6@gmail.com>
2020-12-24 15:25:29 +0100

    Add js & scss Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,18 @@
 import React from "react";
-
+import IcoArrow from "../../images/ico-arrow.svg";
 
 const Footer = (props) => {
  
   return (
     <footer className="footer" role="contentinfo">
-     
+      
+     {/* <div class="footer__button">
+        <a className="footer__button--link" href="#top" rel="Inicio" title="Ir al inicio de la página">
+            <img src={IcoArrow} alt="Ir al inicio de la página" title="Enlace a inicio" />
+        </a>
+    </div>  */}
+ 
+    <small className= "footer__small">&copy; 2020. Ana Martínez Fernández</small>
   </footer>
   );
 };
